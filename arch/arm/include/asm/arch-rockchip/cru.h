d60e3e4ffc0c8a358a010e26dd00e46fce6689ad
Jagan Teki <jagan@amarulasolutions.com>
2019-08-26 00:42:25 +0530

    arm: rockchip: Add common cru.h
    Few of the rockchip family SoC atleast rk3288,
    rk3399 are sharing some cru register bits so
    adding common code between these SoC families
    would require to include both cru include files
    that indeed resulting function declarations error.
    
    So, create a common cru include as cru.h then
    include the rk3399 arch cru include file and move
    the common cru register bit definitions into it.
    
    The rest of rockchip cru files will add it in future.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

---
+++ arch/arm/include/asm/arch-rockchip/cru.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * (C) Copyright 2019 Amarula Solutions.
+ * Author: Jagan Teki <jagan@amarulasolutions.com>
+ */
+
+#ifndef _ROCKCHIP_CLOCK_H
+#define _ROCKCHIP_CLOCK_H
+
+#if defined(CONFIG_ROCKCHIP_RK3399)
+# include <asm/arch-rockchip/cru_rk3399.h>
+#endif
+
+#endif /* _ROCKCHIP_CLOCK_H */
